12a0d6e8bd65b135f86cab6718a3953bac07a936
2132465465465465464k#d <42399712+kimo-dmit@users.noreply.github.com>
2018-08-20 09:44:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,18 @@ message.channel.send({embed:embed}).then(message => message.delete(5000));
 }
 });
 
+client.on('message', message => {
+     if (message.content === prefix + "bot") {
+
+message.channel.send(`
+
+__Servers : ${client.guilds.size}__
+__Users : ${client.users.size}__
+__Channels : ${client.channels.size}__
+
+`);
+    }
+});
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
   console.log(`Servers : ${client.guilds.size} `);
@@ -47,6 +59,73 @@ client.on('ready', () => {
   console.log(`Channels : ${client.channels.size}`);
   
    client.user.setActivity('Soon',{type: 'WATCHING'})
+});
+
+client.on('message', message => {
+  var prefix = "."
+        if (message.content.startsWith(prefix + "uptime")) {
+    let ms = client.uptime;
+    let cd = 24 * 60 * 60 * 1000; 
+    let ch = 60 * 60 * 1000; 
+    let cm = 60 * 1000; 
+    let cs = 1000; 
+    let days = Math.floor(ms / cd);
+    let dms = days * cd; 
+    let hours = Math.floor((ms - dms) / ch);
+    let hms = hours * ch; 
+    let minutes = Math.floor((ms - dms - hms) / cm);
+    let mms = minutes * cm; 
+    let seconds = Math.round((ms - dms - hms - mms) / cs);
+    if (seconds === 60) {
+        minutes++; 
+        seconds = 0;
+    }
+    if (minutes === 60) {
+        hours++; 
+        minutes = 0;
+    }
+    if (hours === 24) {
+        days++; 
+        hours = 0;
+    }
+    let dateStrings = [];
+
+    if (days === 1) {
+        dateStrings.push('**1** day');
+    } else if (days > 1) {
+        dateStrings.push('**' + String(days) + '** days');
+    }
+
+    if (hours === 1) {
+        dateStrings.push('**1** hour');
+    } else if (hours > 1) {
+        dateStrings.push('**' + String(hours) + '** hours');
+    }
+
+    if (minutes === 1) {
+        dateStrings.push('**1** minute');
+    } else if (minutes > 1) {
+        dateStrings.push('**' + String(minutes) + '** minutes');
+    }
+
+    if (seconds === 1) {
+        dateStrings.push('**1** second');
+    } else if (seconds > 1) {
+        dateStrings.push('**' + String(seconds) + '** seconds');
+    }
+
+    let dateString = '';
+    for (let i = 0; i < dateStrings.length - 1; i++) {
+        dateString += dateStrings[i];
+        dateString += ', ';
+    }
+    if (dateStrings.length >= 2) {
+        dateString = dateString.slice(0, dateString.length - 2) + dateString.slice(dateString.length - 1);
+        dateString += 'and ';
+    }
+    dateString += dateStrings[dateStrings.length - 1];
+    message.channel.send(dateString);
+}
 });
 
 client.on('guildMemberAdd', member => {
